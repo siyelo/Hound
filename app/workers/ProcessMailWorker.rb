486718a244ebde8a61d3aa92e1d6acbbb8ca2031
Peter Garbers <peter.garbers@gmail.com>
2012-01-06 18:14:37 +0200

    untracked files

---
+++ app/workers/ProcessMailWorker.rb
@@ -0,0 +1,13 @@
+class ProcessMailWorker
+  # Here we will have the mail processing
+  # This will be responsible for checking
+  # whether a user has an account or enough
+  # email credits to send an email
+
+  @queue = :process_queue
+  def self.perform(mail)
+    debugger
+    Resque.enqueue(SendMailWorker, mail)
+
+  end
+end
---
+++ app/workers/SendMailWorker.rb
@@ -0,0 +1,4 @@
+class SendMailWorker
+  # This class is responsible for sending
+  # the mails once they have been processed
+end
