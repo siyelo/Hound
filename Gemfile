effda87bf28491728ddbb7a72b0e84ad194c34e2
Peter Garbers <peter.garbers@gmail.com>
2012-01-06 18:14:27 +0200

    working resque scheduler

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'sqlite3'
 gem 'therubyracer'
 #gem 'resque', :require => "resque/server"
 gem 'resque-scheduler', :require => "resque/server"
-
+#gem 'resque-scheduler'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/workers/FetchMailWorker.rb
+++ app/workers/FetchMailWorker.rb
@@ -1,12 +1,14 @@
 class FetchMailWorker
   require 'net/imap'
 
+  @queue = :fetch_queue
   def fetch_mail
     imap = Net::IMAP.new('imap.gmail.com', 993, true)
     imap.login('radmeet@siyelo.com', 'i4msoc00l')
     imap.search(["ALL"]).each do |message_id|
       envelope = imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
-      Reminder.create!(email: envelope.from[0].name, subject: envelope.subject)
+      reminder = Reminder.create!(email: envelope.from[0].name, subject: envelope.subject)
+      ProcessMailWorker.process_mail(reminder.id)
     end
   end
 end
