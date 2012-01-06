f36c2d5b432ce69999e6eca15e226f8cf952ee87
Peter Garbers <peter.garbers@gmail.com>
2012-01-06 17:02:22 +0200

    Added other worker classes

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Reminder do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :subject }
-
+  describe "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :subject }
+  end
 end
