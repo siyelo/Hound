f92f2723a1443728ef9835a3cc0b8f776275e39d
Peter Garbers <peter.garbers@gmail.com>
2012-01-18 11:03:50 +0200

    cleaned up parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -6,41 +6,30 @@ module EmailParser
       start_date ? start_date + self.reminder_time(email) : self.reminder_time(email).from_now
     end
 
-    def self.reminder_time(email)
-      (self.scan_minutes(email) + self.scan_hours(email) +
-      self.scan_days(email) + self.scan_weeks(email) +
-      self.scan_months(email) +  self.scan_years(email))
-    end
+    private
 
-    def self.scan_minutes(email)
-      minutes = email.scan(/(\d{1,2})(?!mo)m[a-zA-Z]*/)
-      !minutes.empty? ? minutes.first[0].to_i.minutes : 0.minutes
-    end
+      def self.reminder_time(email)
+        [:minutes, :hours, :days, :weeks,
+         :months, :years].inject(0.seconds) do |result, m|
+          result += self.scan_time(email, m)
+        end
+      end
 
-    def self.scan_hours(email)
-      hours = email.scan(/(\d{1,2})ho?u?r?s?/)
-      !hours.empty? ? hours.first[0].to_i.hours : 0.hours
-    end
+      def self.get_regex(measurement)
+        case measurement
+        when :minutes then /(\d{1,2})(?!mo)m[a-zA-Z]*/
+        when :hours then /(\d{1,2})ho?u?r?s?/
+        when :days then /(\d{1,2})da?y?s?/
+        when :weeks then /(\d{1,2})we?e?k?s?/
+        when :months then /(\d{1,2})mo[a-zA-Z]*/
+        when :years then /(\d{1,2})ye?a?r?s?/
+        end
+      end
 
-    def self.scan_days(email)
-      days = email.scan(/(\d{1,2})da?y?s?/)
-      !days.empty? ? days.first[0].to_i.days : 0.days
-    end
-
-    def self.scan_weeks(email)
-      weeks = email.scan(/(\d{1,2})we?e?k?s?/)
-      !weeks.empty? ? weeks.first[0].to_i.weeks : 0.weeks
-    end
-
-    def self.scan_months(email)
-      months = email.scan(/(\d{1,2})mo[a-zA-Z]*/)
-      !months.empty? ? months.first[0].to_i.months : 0.months
-    end
-
-    def self.scan_years(email)
-      years = email.scan(/(\d{1,2})ye?a?r?s?/)
-      !years.empty? ? years.first[0].to_i.years : 0.years
-    end
+      def self.scan_time(email, unit)
+        time = email.scan self.get_regex(unit)
+        !time.empty? ? time.first[0].to_i.send(unit.to_sym) : 0.seconds
+      end
   end
 end
 
