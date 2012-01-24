92b7e10d959fceb616f9851cc8f440634b632b05
Peter Garbers <peter.garbers@gmail.com>
2012-01-24 17:53:33 +0200

    added regex for next(day) ect

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,36 +1,114 @@
 module EmailParser
   class Parser
     require 'active_support/all'
+    # this is going to need a rewrite when we are done
 
     ### Constants
 
-    MEASUREMENT = {
-         minutes:  /(\d{1,2})(?!mo)m[a-zA-Z]*/,
-         hours:  /(\d{1,2})ho?u?r?s?/,
-         days:  /(\d{1,2})da?y?s?/,
-         weeks:  /(\d{1,2})we?e?k?s?/,
-         months:  /(\d{1,2})mo[a-zA-Z]*/,
-         years:  /(\d{1,2})ye?a?r?s?/
+    TIME = {
+      tomorrow: /tomorrow/,
+      endofday: /endofday/,
+      endofmonth: /endofmonth/,
+      #make this regex match the day
+      nextmonday: /nextmonday/,
+      nexttuesday: /nexttuesday/,
+      nextwednesday: /nextwednesday/,
+      nextthursday: /nextthursday/,
+      nextfriday: /nextfriday/,
+      nextsaturday: /nextsaturday/,
+      nextsunday: /nextsunday/,
+      nextweek: /nextweek/
+    }
+
+    INCREMENTS = {
+      minutes:  /(\d{1,2})(?!mo)m[a-zA-Z]*/,
+      hours:  /(\d{1,2})ho?u?r?s?/,
+      days:  /(\d{1,2})da?y?s?/,
+      weeks:  /(\d{1,2})we?e?k?s?/,
+      months:  /(\d{1,2})mo[a-zA-Z]*/,
+      years:  /(\d{1,2})ye?a?r?s?/
     }
 
     ### Class methods
 
     def self.parse_email(email, start_date = nil)
-      start_date ? start_date + self.reminder_time(email) : self.reminder_time(email).from_now
+      matches = self.match_time_words(email)
+      unless matches.empty?
+        self.send(matches.first)
+      else
+        start_date ? start_date + self.reminder_time(email) : self.reminder_time(email).from_now
+      end
     end
 
     private
 
-      def self.reminder_time(email)
-        MEASUREMENT.keys.inject(0.seconds) do |result, m|
-          result += self.scan_time(email, m)
-        end
-      end
+    def self.match_time_words(email)
+      TIME.keys.inject([]) { |result, m| result << email.scan(TIME[m]) }.flatten
+    end
 
-      def self.scan_time(email, unit)
-        time = email.scan MEASUREMENT[unit]
-        time.empty? ? 0.seconds : time.first[0].to_i.send(unit.to_sym)
+    def self.reminder_time(email)
+      INCREMENTS.keys.inject(0.seconds) do |result, m|
+        result += self.scan_increments(email, m)
       end
+    end
+
+    def self.scan_increments(email, unit)
+      time = email.scan INCREMENTS[unit]
+      time.empty? ? 0.seconds : time.first[0].to_i.send(unit.to_sym)
+    end
+
+    def self.tomorrow
+      1.day.from_now.change hour: 8
+    end
+
+    def self.endofday
+      Time.now.change hour: 17
+    end
+
+    def self.endofmonth
+      Time.now.end_of_month.change hour: 8
+    end
+
+
+    def self.date_of_next(day)
+      date  = DateTime.parse(day)
+      delta = date > Date.today ? 0 : 7
+      (date + delta).change hour: 8
+    end
+
+    def self.nextmonday
+      date_of_next("monday")
+    end
+
+    def self.nexttuesday
+      date_of_next("tuesday")
+    end
+
+    def self.nextwednesday
+      date_of_next("wednesday")
+    end
+
+    def self.nextthursday
+      date_of_next("thursday")
+    end
+
+    def self.nextfriday
+      date_of_next("friday")
+    end
+
+    def self.nextsaturday
+      date_of_next("saturday")
+    end
+
+    def self.nextsunday
+      date_of_next("sunday")
+    end
+
+    def self.nextweek
+      Time.now.next_week.change hour: 8
+    end
+
+
   end
 end
 
@@ -40,3 +118,4 @@ end
 #day d
 #hour \d+h
 #minute \d+m
+#tomorrow
--- spec/models/parser_spec.rb
+++ spec/models/parser_spec.rb
@@ -6,6 +6,37 @@ describe EmailParser::Parser do
       Timecop.freeze
     end
 
+    context "tomorrow" do
+      it "should return 8am the next day" do
+        email = "tomorrow@sorad.cc"
+        EmailParser::Parser.parse_email(email).should == 1.day.from_now.change(hour: 8)
+      end
+    end
+
+    context "nextday" do
+      it "should return the next day (mon tues ect)" do
+        email = "nextmonday@sorad.cc"
+        EmailParser::Parser.parse_email(email).wday.should == 1
+        EmailParser::Parser.parse_email(email).hour.should == 8
+      end
+    end
+
+    context "endofday" do
+      it "should return 8am the next day" do
+        email = "endofday@sorad.cc"
+        EmailParser::Parser.parse_email(email).should == Time.now.change(hour: 17)
+      end
+    end
+
+    context "endofmonth" do
+      it "should return 8am the last day of the month" do
+        email = "endofmonth@sorad.cc"
+        t = Time.now
+        resulting_time = (t.change month: t.month + 1, day: 1, hour: 8) - 1.day
+        EmailParser::Parser.parse_email(email).should == resulting_time
+      end
+    end
+
     context "minutes" do
       it "should detect the minutes" do
         email = "2minutes@sorad.cc"
@@ -92,19 +123,19 @@ describe EmailParser::Parser do
       it "should detect everything" do
         email = "2years3months12days23hours1minute@sorad.cc"
         EmailParser::Parser.parse_email(email).should == (2.years + 3.months + 12.days +
-        23.hours + 1.minute).from_now
+                                                          23.hours + 1.minute).from_now
       end
 
       it "should work with composite times" do
         email = "2y3mo12d23h1m@sorad.cc"
         EmailParser::Parser.parse_email(email).should == (2.years + 3.months + 12.days +
-        23.hours + 1.minute).from_now
+                                                          23.hours + 1.minute).from_now
       end
 
       it "should work with a mixture of composite and non times" do
         email = "2years3mo12days23h1min@sorad.cc"
         EmailParser::Parser.parse_email(email).should == (2.years + 3.months + 12.days +
-        23.hours + 1.minute).from_now
+                                                          23.hours + 1.minute).from_now
       end
     end
   end
