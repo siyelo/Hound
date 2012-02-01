d324eee7ac5ea05ed3528902cf0d29ececdeb01a
Peter Garbers <peter.garbers@gmail.com>
2012-02-01 17:17:52 +0200

    changed to hound@hound.cc email address

--- config/initializers/default_mailer.rb
+++ config/initializers/default_mailer.rb
@@ -1,7 +1,7 @@
 Mail.defaults do
   retriever_method :pop3, :address    => "pop.gmail.com",
                           :port       => 995,
-                          :user_name  => 'radmeet@mailshotbot.com',
-                          :password   => 'si@yelo321',
+                          :user_name  => 'hound@hound.cc',
+                          :password   => 'houndyhound123',
                           :enable_ssl => true
 end
