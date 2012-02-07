2c72553c1747d68dc498363ed34e53f1222af382
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-02-07 14:03:10 +0200

    fallback to asset pipeline if precompiled asset is missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 RadMeet::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_mailer.default_url_options = { :host => 'mrpink.siyelo.com:8000' }
+  config.action_mailer.default_url_options = { :host => 'www.hound.cc' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -15,7 +15,7 @@ RadMeet::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
