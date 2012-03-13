91a4a46f28de186bab6a9909d2ec2f519c91619c
Glenn Roberts <glenn.roberts@siyelo.com>
2012-03-13 11:03:49 +0200

    green! fuck yeah!

--- app/models/date_time.rb
+++ app/models/date_time.rb
@@ -1,5 +1,6 @@
 class DateTime
   def self.parse_email(address)
+    #http://en.wikipedia.org/wiki/Email_address#Local_part
     local_part = address.split('@')[0] 
 
     send_at = if local_part.match /(?!months)[a-z]{6,10}/
