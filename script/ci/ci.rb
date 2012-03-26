59f4106fcbf19efc6583f1e24bb2801fdcef2540
Glenn Roberts and Sachin Ranchod <dev+glenn+sachin@siyelo.com>
2012-03-26 16:53:02 +0200

    run xvfb before full ci

--- script/ci/ci.rb
+++ script/ci/ci.rb
@@ -32,23 +32,23 @@ def run_specs
   run_or_die "rake test"
 end
 
-# http://blog.kabisa.nl/2010/05/24/headless-cucumbers-and-capybaras-with-selenium-and-hudson/
-# and http://markgandolfo.com/2010/07/01/hudson-ci-server-running-cucumber-in-headless-mode-xvfb
-# def setup_x_server
-#   ENV['RAILS_ENV'] = 'test'
-#   ENV['DISPLAY'] = ":99"
-#   run "/etc/init.d/xvfb start"
-# end
+#http://blog.kabisa.nl/2010/05/24/headless-cucumbers-and-capybaras-with-selenium-and-hudson/
+#and http://markgandolfo.com/2010/07/01/hudson-ci-server-running-cucumber-in-headless-mode-xvfb
+def setup_x_server
+  ENV['RAILS_ENV'] = 'test'
+  ENV['DISPLAY'] = ":99"
+  run "/etc/init.d/xvfb start"
+end
 
-# def teardown_x_server
-#   run "/etc/init.d/xvfb stop"
-# end
+def teardown_x_server
+  run "/etc/init.d/xvfb stop"
+end
 
 # main
 run_or_die "bundle install"
 setup_db
-#setup_x_server
+setup_x_server
 run_specs
-#teardown_x_server
+teardown_x_server
 run_or_die "rake clean"
 
