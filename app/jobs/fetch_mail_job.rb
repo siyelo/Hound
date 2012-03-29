922725d9e78f5cb78901b62e593c809e1d1243a3
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-03-29 10:00:07 +0200

    fix fetch_mail_job_spec leaking

--- app/jobs/fetch_mail_job.rb
+++ app/jobs/fetch_mail_job.rb
@@ -1,7 +1,9 @@
+require 'reminder_creation_service'
+
 class FetchMailJob
   # This class is responsible for fetching the emails.
   # It then delegates any further actions to the other classes
-  
+
   @queue = :fetch_queue
 
   def self.perform
