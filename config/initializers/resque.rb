2b05d8fb855b74e29d4f75f747e61a6cc3e474c3
Sachin Ranchod <sachin.ranchod@gmail.com>
2012-03-30 13:56:11 +0200

    resque errors now throw Airbrakes

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,13 @@
 require 'resque_scheduler'
+require 'resque/failure/multiple'
+require 'resque/failure/airbrake'
+require 'resque/failure/redis'
+
+Resque::Failure::Airbrake.configure do |config|
+  config.api_key = '5e5cd1f814ba7bbe61aa9a756b1b7f56'
+  config.secure = true
+end
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+Resque::Failure.backend = Resque::Failure::Multiple
+
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
