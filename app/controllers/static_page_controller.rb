4e2667beaaf8fb198afc8802da380fff2510da68
Glenn Roberts <glenn.roberts@siyelo.com>
2012-04-03 14:29:02 +0200

    fix menus, remove redundant layout

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,3 @@
 class StaticPageController < ApplicationController
-  layout 'static_page'
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def cp(path)
     "active" if current_page?(path) || current_page?(action: path)
   end
+
+  def landing_page?
+    current_page?(controller: :static_page, action: :index)
+  end
 end
