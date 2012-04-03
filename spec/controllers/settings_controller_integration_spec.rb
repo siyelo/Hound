323074d1c605059af8a7e54def1b44842359c7c0
Glenn Roberts <glenn.roberts@siyelo.com>
2012-04-03 13:39:25 +0200

    add specs for error workflows on settings update, remove unused devise_mapping from settings helper

--- spec/controllers/settings_controller_integration_spec.rb
+++ spec/controllers/settings_controller_integration_spec.rb
@@ -13,23 +13,44 @@ describe SettingsController do
       response.should be_success
     end
 
-    it "updates without password" do
-      put :update, id: user.id, user: {'email' => 'g1@g.com'}
-      flash[:notice].should == "You have successfully updated your settings."
-      response.should redirect_to settings_path
+    describe "#update" do
+      it "updates without password" do
+        put :update, id: user.id, user: {'email' => 'g1@g.com'}
+        flash[:notice].should == "You have successfully updated your settings."
+        response.should redirect_to settings_path
+      end
+
+      it "updates, ignoring password" do
+        put :update, id: user.id, user: {'password' => 'abcdef'}
+        flash[:notice].should == "You have successfully updated your settings."
+        response.should redirect_to settings_path
+      end
+
+      it "re-renders on failed update" do
+        user.stub(:update_without_password).and_return false
+        User.stub(:find).and_return(user)
+        put :update, id: user.id, user: {'email' => 'g1@g.com'}
+        flash[:alert].should == "Sorry, we couldn't update your settings."
+        response.should render_template 'edit'
+      end
     end
 
-    it "updates, ignoring password" do
-      put :update, id: user.id, user: {'password' => 'abcdef'}
-      flash[:notice].should == "You have successfully updated your settings."
-      response.should redirect_to settings_path
-    end
+    describe "#update_password" do
+      it "updates password" do
+        put :update_password, id: user.id, user: { 'current_password' => 'testing',
+          'password' => 'abcdef' }
+        response.should redirect_to settings_path
+        flash[:notice].should == "You have successfully updated your password."
+      end
 
-    it "updates password" do
-      put :update_password, id: user.id, user: { 'current_password' => 'testing',
-        'password' => 'abcdef' }
-      flash[:notice].should == "You have successfully updated your password."
-      response.should redirect_to settings_path
+      it "re-renders on failed update" do
+        user.stub(:update_with_password).and_return false
+        User.stub(:find).and_return(user)
+        put :update_password, id: user.id, user: { 'current_password' => 'testing',
+          'password' => 'abcdef' }
+        response.should render_template 'edit'
+        flash[:alert].should == "Sorry, we couldn't update your password."
+      end
     end
   end
 
