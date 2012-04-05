d6a1921787561a8a38bdbf4e49bf2e1989cd4676
Peter Garbers <peter.garbers@gmail.com>
2012-04-05 15:57:00 +0200

    fixed disable confirmation error

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,7 +2,7 @@ class ConfirmationsController < ApplicationController
 
   def disable
     u = User.find_by_modify_token(params[:token])
-    if u && u.disable_confirmation_emails(params[:token])
+    if u && u.disable_confirmation_emails
       render action: 'success'
     else
       render action: 'failed'
