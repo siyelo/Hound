0e17b7b07e8977d29b79136ac7116de8a31e9069
Peter Garbers <peter.garbers@gmail.com>
2012-06-05 12:09:50 +0200

    [Fixes #29017015] Updated sendgrid details

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -3,8 +3,8 @@ if Rails.env == "production"
   ActionMailer::Base.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => "petersiyelo",
-    :password => "dkObxYKEi94YKq",
+    :user_name => "siyelo",
+    :password => "yHSeIRpYjiR4XDc2Gx",
     :domain => "sorad.cc",
     :address => "smtp.sendgrid.net",
     :port => 587,
