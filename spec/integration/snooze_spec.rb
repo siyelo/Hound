bbd72174cd2b720917a5bc36b59644603abaccb0
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-06-07 13:48:37 +0200

    [fixes #29978469] Improve wording on snooze page

--- spec/integration/snooze_spec.rb
+++ spec/integration/snooze_spec.rb
@@ -8,22 +8,25 @@ describe "Snooze", type: :request do
   end
 
   it "allows user to snooze and notify participants" do
-    reminder = Factory :reminder, user: @user, fetched_mail: Factory(:fetched_mail, from: 'pimp@macdaddy.yo')
+    reminder = Factory :reminder, user: @user,
+      other_recipients: ['d@d.com', 'g@g.com'],
+      fetched_mail: Factory(:fetched_mail, from: 'pimp@macdaddy.yo')
     Hound::Notifier.send_reminders(reminder)
     open_email(@user.email)
     visit_in_email('15min')
     page.should have_content("15 minutes from now")
-    page.should have_content("Would you like to notify the other recipients")
+    page.should have_content("Would you like to notify the other recipients (d@d.com, g@g.com) that you've snoozed the reminder?")
   end
 
   it "does not show option to notify if there are no other participants" do
     reminder = Factory :reminder, user: @user, other_recipients: [],
+      other_recipients: ['d@d.com'],
       fetched_mail: Factory(:fetched_mail, from: 'pimp@macdaddy.yo')
     Hound::Notifier.send_reminders(reminder)
     open_email(@user.email)
     visit_in_email('12h')
     page.should have_content("12 hours from now")
-    page.should_not have_content("Would you like to notify the other recipients")
+    page.should have_content("Would you like to notify the other recipients (d@d.com) that you've snoozed the reminder?")
   end
 end
 
