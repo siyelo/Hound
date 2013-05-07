2531e32d35a63f17e194a32841fcd19dfddb5a7d
Sachin Ranchod <sachin.ranchod@gmail.com>
2013-05-07 16:54:46 +0200

    remove unnecessary unicorn / nginx config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,1 @@
-working_directory "/home/hound/app"
-pid "/home/hound/app/tmp/pids/unicorn.pid"
-stderr_path "/home/hound/app/log/unicorn.log"
-stdout_path "/home/hound/app/log/unicorn.log"
-
-listen "/tmp/unicorn.hound_app.sock"
 worker_processes 3
