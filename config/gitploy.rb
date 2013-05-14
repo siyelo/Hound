089bdb65705c766ab704a0544ad726b47078b293
Sachin Ranchod <sachin.ranchod@gmail.com>
2013-05-14 17:06:42 +0200

    update gitploy and rbenv version

--- config/gitploy.rb
+++ config/gitploy.rb
@@ -1,11 +1,11 @@
 require 'gitploy/script'
 
 configure do |c|
-  c.path = '/home/hound/app'
+  c.path = '/home/ubuntu/app'
 
   stage :production do
-    c.host = 'butch.siyelo.com'
-    c.user = 'hound'
+    c.host = 'ec2-54-245-208-6.us-west-2.compute.amazonaws.com'
+    c.user = 'ubuntu'
   end
 end
 
@@ -26,14 +26,14 @@ deploy do
     run 'export PATH="$HOME/.rbenv/bin:$PATH"'
     run 'eval "$(rbenv init -)"'
     run "ruby -v"
-    run "bundle install"
-    run "rake db:migrate"
-    run "bundle exec rake assets:precompile"
-    run "bundle exec foreman export upstart hound_scripts -a hound -u hound"
+    run "bundle install --deployment --without development, testing"
+    run "bundle exec foreman export upstart hound_scripts -a hound -u ubuntu"
     run "sudo cp hound_scripts/* /etc/init/"
     run "rm -rf hound_scripts"
     run "sudo sed -i \"1 i start on runlevel [2345]\" /etc/init/hound.conf"
     run "sudo /etc/init.d/hound_app restart"
+    run "rbenv sudo rake db:migrate"
+    run "rbenv sudo bundle exec rake assets:precompile"
     run "echo FINISHED."
   end
 end
